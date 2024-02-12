24d3b5e8c36e9d75d7c07462da3d4e94348f5399
Pragadeesh Nehru <pragadeeshxenocrate@gmail.com>
2024-02-13 00:38:48 +0530

    Database connected

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+var mongoURL =
+  "mongodb+srv://pragadeeshnehru:<7917145518@Jla>@cluster0.l5eiz8h.mongodb.net/";
+
+mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParse: true });
+var connection = mongoose.connection;
+
+connection.on("error", () => {
+  console.log("DB Connection Failed");
+});
+
+connection.on("connected", () => {
+  console.log("DB Connection Sucessful");
+});

e784e33baca7a6a7d2fdda143d821e7191154532
Pragadeesh Nehru <pragadeeshxenocrate@gmail.com>
2024-02-17 13:05:42 +0530

    Revert "version updates"
    This reverts commit bce90cf769c36802ea794b17ccb12d143b81f495.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
-import HomeScreen from "./screens/HomeScreen";
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/home" exact component={<HomeScreen/>} />
-        </Routes>
-      </BrowserRouter>
+      <Navbar/>
+      yarn add react-router-dom
     </div>
   );
 }
